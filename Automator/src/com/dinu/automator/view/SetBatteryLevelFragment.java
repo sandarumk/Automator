03b7c09c358b182cd7b768ab40494d81d1300619
sandarumk <sandarumk@gmail.com>
2013-09-25 15:37:47 +0530

    Added widget manually coded double sided seek bar

--- Automator/src/com/dinu/automator/view/SetBatteryLevelFragment.java
+++ Automator/src/com/dinu/automator/view/SetBatteryLevelFragment.java
@@ -1,13 +1,23 @@
 package com.dinu.automator.view;
 
+import android.app.Activity;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.dinu.automator.BatteryLevel;
 import com.dinu.automator.R;
+import com.dinu.automator.RangeSeekBar;
+import com.dinu.automator.RangeSeekBar.OnRangeSeekBarChangeListener;
+import com.dinu.automator.activity.ProfileSettingsActivity;
 
 public class SetBatteryLevelFragment extends Fragment{
 	private static SetBatteryLevelFragment batry;
@@ -24,9 +34,22 @@ public BatteryLevel getBattryLevel() {
 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		View view = inflater.inflate(R.layout.battery_level_fragment, null);
+	
+		RangeSeekBar<Integer> seekBar = new RangeSeekBar<Integer>(20, 75, getActivity());
+		seekBar.setOnRangeSeekBarChangeListener(new OnRangeSeekBarChangeListener<Integer>() {
+		        @Override
+		        public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
+		                // handle changed range values
+		                
+		        }
+		});
+		ViewGroup layout = (ViewGroup)view.findViewById(R.id.linearlayout_battery_level_fragment);
+		layout.addView(seekBar);
 		return view;
 		
 	}
+
+
 
 public static SetBatteryLevelFragment getInstance() {
 	if (batry==null){
